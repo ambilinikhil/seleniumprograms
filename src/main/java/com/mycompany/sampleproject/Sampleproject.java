e2f94840de6cc558939341b16627deb684c4c233
LENOVO <LENOVO@DESKTOP-CH8IFGA>
2025-12-23 09:57:44 +0530

    My Project Files

---
+++ src/main/java/com/mycompany/sampleproject/Sampleproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sampleproject;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Sampleproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
